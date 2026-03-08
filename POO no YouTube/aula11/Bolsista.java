5d97f3664aa537b8f7895b678169e41cccb9f6e1
Edgar Neto <edgarneto5508@gmail.com>
2026-03-07 21:26:11 -0300

    Aula11

---
+++ POO no YouTube/aula11/Bolsista.java
@@ -0,0 +1,22 @@
+package aula11;
+
+public class Bolsista extends Aluno{
+    private float bolsa;
+
+    public void renovarBolsa(){
+        System.out.println("Renovando bolsa de " + this.getNome());
+    }
+
+    @Override
+    public void pagarMesalidade(){
+        System.out.println(this.getNome() + " é bolsista pagar mesalidade facilitada.");
+    }
+
+    public float getBolsa() {
+        return bolsa;
+    }
+
+    public void setBolsa(float bolsa) {
+        this.bolsa = bolsa;
+    }
+}
